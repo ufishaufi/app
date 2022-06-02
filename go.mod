c3f9a3b8422c12e303ef40cb24ce4f9a781296fd
Muhammad Shaufi Imanulhaq <shaufiman@gmail.com>
2022-06-03 05:21:09 +0800

    menambah dependency

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ufishaufi/app
+
+go 1.18
+
+require github.com/ufishaufi/module v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	hello "github.com/ufishaufi/module"
+)
+
+func main() {
+	fmt.Println(hello.SayHello())
+}
