5a051c96ac83dd5b27b8f8261e96c516230d4b45
Muhammad Shaufi Imanulhaq <shaufiman@gmail.com>
2024-01-21 08:44:17 +0800

    Project is completed

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/ufishaufi/app
 
 go 1.20
 
-require github.com/ufishaufi/module v1.5.0
+require github.com/ufishaufi/module/v2 v2.0.0
--- main.go
+++ main.go
@@ -3,10 +3,10 @@ package main
 import (
 	"fmt"
 
-	"github.com/ufishaufi/module"
+	"github.com/ufishaufi/module/v2"
 )
 
 func main() {
 
-	fmt.Println(module.SayHello())
+	fmt.Println(module.SayHello("Shaufi"))
 }
